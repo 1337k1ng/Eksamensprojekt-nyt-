4cc9f372ce99277796aa93cc93f7bf0a1ea669ce
1337k1ng <Madsjohanbech@live.dk>
2021-01-26 23:25:48 +0100

    travis

---
+++ src/main/java/utils/TestUser.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utils;
+
+import entities.Role;
+import entities.User;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+
+/**
+ *
+ * @author Madsj
+ */
+public class TestUser {
+    /*
+    static EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
+    static EntityManager em = emf.createEntityManager();
+    
+    public static void main(String[] args) {
+      User user1 = new User("John", "kimbo");
+      user1.addRole(new Role("admin"));
+      
+      em.getTransaction().begin();
+      em.persist(new Role("admin"));
+      em.persist(user1);
+      em.getTransaction().commit();
+      em.close();
+      
+    }
+   
+    */
+}
