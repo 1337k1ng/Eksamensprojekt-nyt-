15ccf4c336a9d980a03370e0063ba0e7dc078de0
1337k1ng <Madsjohanbech@live.dk>
2021-01-28 01:29:41 +0100

    første commit

---
+++ src/main/java/dto/ClassesDTO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+import entities.Classs;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Madsj
+ */
+public class ClassesDTO {
+
+    List<ClasssDTO> allClasses = new ArrayList();
+
+    public ClassesDTO(List<Classs> classes) {
+
+        classes.forEach((c) -> {
+            allClasses.add(new ClasssDTO(c));
+        });
+
+    }
+
+}
